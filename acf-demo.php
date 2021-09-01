f5f8fe5e09c3074acf0a657a93080a51bed0f4ca
Mahadi001 <sadat.himel@gmail.com>
2021-09-01 21:44:48 +0600

    update

--- acf-demo.php
+++ acf-demo.php
@@ -16,3 +16,44 @@ function acfd_bootstrap(){
     load_plugin_textdomain("acf-demo",false,dirname(__FILE__)."/languages");
 }
 add_action( 'plugin_loaded', 'acfd_bootstrap');
+
+add_action( 'tgmpa_register', 'acfd_tgm_register_required_plugins' );
+
+
+function acfd_tgm_register_required_plugins() {
+
+	$plugins = array(
+
+		// This is an example of how to include a plugin from the WordPress Plugin Repository.
+		array(
+			'name'      => 'BuddyPress',
+			'slug'      => 'buddypress',
+			'required'  => false,
+		),
+
+		
+		array(
+			'name'        => 'WordPress SEO by Yoast',
+			'slug'        => 'wordpress-seo',
+			'is_callable' => 'wpseo_init',
+		),
+
+	);
+
+	
+	$config = array(
+		'id'           => 'acf-demo',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'plugins.php',            // Parent menu slug.
+		'capability'   => 'manage_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+
+	);
+
+	tgmpa( $plugins, $config );
+}
